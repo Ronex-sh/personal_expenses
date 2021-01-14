e96a176a5bacb9f11157aece8687a8c7b0e9b14c
karrarsaad <kararmax124@gmail.com>
2021-01-14 23:17:38 +0300

    add EditingController

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
-
-
 import 'package:flutter/material.dart';
 import 'package:personal_expenses/transaction.dart';
 import 'package:intl/intl.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -16,7 +15,7 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
- final List<Transaction> transactions = [
+  final List<Transaction> transactions = [
     Transaction(
       id: 't1',
       title: 'New Shoes',
@@ -30,6 +29,9 @@ class MyHomePage extends StatelessWidget {
       date: DateTime.now(),
     ),
   ];
+  //String titleInput, amontInput;
+  final titleController = TextEditingController();
+  final amountController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -37,8 +39,7 @@ class MyHomePage extends StatelessWidget {
         title: Text('Flutter App'),
       ),
       body: Column(
-         mainAxisAlignment: MainAxisAlignment.spaceAround,
-        
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
           Container(
             width: double.infinity,
@@ -48,54 +49,87 @@ class MyHomePage extends StatelessWidget {
               elevation: 5,
             ),
           ),
+          Card(
+            elevation: 25,
+            child: Container(
+              margin: EdgeInsets.all(10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  TextField(
+                    controller: titleController,
+                      decoration: InputDecoration(labelText: 'Title'),
+                     // onChanged: (v) => titleInput = v
+                     ),
+                  TextField(
+                    controller: amountController,
+                      decoration: InputDecoration(labelText: 'Amount'),
+                   //   onChanged: (v) => amontInput = v
+                   ),
+                  FlatButton(
+                    onPressed: () {
+                      print(titleController.text);
+                    },
+                    child: Text('Add Transaction'),
+                    color: Colors.amber,
+                  )
+                ],
+              ),
+            ),
+          ),
           // Card(child: Text(transactions[0].title),)  // to display the transactions >> static
           Column(
-           
-            children: transactions.map((e) => // to display the transactions >> dynamic
-             Row(
-               
-               children: [
-                 Container(
-                  
-                    margin: EdgeInsets.symmetric(
-                        vertical: 10,
-                        horizontal: 15,
-                      ),
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                          color: Colors.purple,
-                          width: 2,
+            children: transactions
+                .map((e) => // to display the transactions >> dynamic
+                    Row(
+                      children: [
+                        Container(
+                          margin: EdgeInsets.symmetric(
+                            vertical: 10,
+                            horizontal: 15,
+                          ),
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                              color: Colors.purple,
+                              width: 2,
+                            ),
+                          ),
+                          padding: EdgeInsets.all(10),
+                          child: Text(
+                            '\$' + e.amount.toString(),
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                              color: Colors.purple,
+                            ),
+                          ),
                         ),
-                      ),
-                      padding: EdgeInsets.all(10),
-                   child: Text('\$'+e.amount.toString(),
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20,
-                          color: Colors.purple,
-                        ),),
-                 ),
-                 Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                   children: [
-                     Text(e.title,
-                          style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold,
-                          ),),
-                    //  Text(e.date.toString(),
-                    //       style: TextStyle(
-                    //         color: Colors.grey,
-                    //       ),)
-                     Text(DateFormat().format(e.date),//add dateFormat
-                          style: TextStyle(
-                            color: Colors.grey,
-                          ),)
-                   ],
-                 )
-               ],
-             ) 
-            ).toList(),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              e.title,
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            //  Text(e.date.toString(),
+                            //       style: TextStyle(
+                            //         color: Colors.grey,
+                            //       ),)
+                            Text(
+                              DateFormat.yMd().format(
+                                  e.date), //add intl package to format the date
+                              style: TextStyle(
+                                color: Colors.grey,
+                              ),
+                            )
+                          ],
+                        )
+                      ],
+                    ))
+                .toList(),
           ),
         ],
       ),
