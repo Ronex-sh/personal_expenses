766ab5972d90ac2336c9b764a0d5f80a27fa2f18
karrarsaad <kararmax124@gmail.com>
2021-01-14 03:14:47 +0300

    add intl(package) to formate the date

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+
+
 import 'package:flutter/material.dart';
 import 'package:personal_expenses/transaction.dart';
-
+import 'package:intl/intl.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -35,6 +37,8 @@ class MyHomePage extends StatelessWidget {
         title: Text('Flutter App'),
       ),
       body: Column(
+         mainAxisAlignment: MainAxisAlignment.spaceAround,
+        
         children: <Widget>[
           Container(
             width: double.infinity,
@@ -46,8 +50,51 @@ class MyHomePage extends StatelessWidget {
           ),
           // Card(child: Text(transactions[0].title),)  // to display the transactions >> static
           Column(
+           
             children: transactions.map((e) => // to display the transactions >> dynamic
-             Card(child: Text(e.title),) 
+             Row(
+               
+               children: [
+                 Container(
+                  
+                    margin: EdgeInsets.symmetric(
+                        vertical: 10,
+                        horizontal: 15,
+                      ),
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.purple,
+                          width: 2,
+                        ),
+                      ),
+                      padding: EdgeInsets.all(10),
+                   child: Text('\$'+e.amount.toString(),
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: Colors.purple,
+                        ),),
+                 ),
+                 Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                   children: [
+                     Text(e.title,
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),),
+                    //  Text(e.date.toString(),
+                    //       style: TextStyle(
+                    //         color: Colors.grey,
+                    //       ),)
+                     Text(DateFormat().format(e.date),//add dateFormat
+                          style: TextStyle(
+                            color: Colors.grey,
+                          ),)
+                   ],
+                 )
+               ],
+             ) 
             ).toList(),
           ),
         ],
