f60a60cacfd96b1d9a9638b54a3afa2aa4478b2f
karrarsaad <kararmax124@gmail.com>
2021-01-15 01:05:31 +0300

    trans_list(update)

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:personal_expenses/model/transaction.dart';
-import 'package:intl/intl.dart';
+import 'package:personal_expenses/widjet/transaction_list.dart';
 
 void main() => runApp(MyApp());
 
@@ -15,20 +14,7 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-  final List<Transaction> transactions = [
-    Transaction(
-      id: 't1',
-      title: 'New Shoes',
-      amount: 69.99,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: 't2',
-      title: 'Weekly Groceries',
-      amount: 16.53,
-      date: DateTime.now(),
-    ),
-  ];
+  
   //String titleInput, amontInput;
   final titleController = TextEditingController();
   final amountController = TextEditingController();
@@ -77,60 +63,8 @@ class MyHomePage extends StatelessWidget {
               ),
             ),
           ),
-          // Card(child: Text(transactions[0].title),)  // to display the transactions >> static
-          Column(
-            children: transactions
-                .map((e) => // to display the transactions >> dynamic
-                    Row(
-                      children: [
-                        Container(
-                          margin: EdgeInsets.symmetric(
-                            vertical: 10,
-                            horizontal: 15,
-                          ),
-                          decoration: BoxDecoration(
-                            border: Border.all(
-                              color: Colors.purple,
-                              width: 2,
-                            ),
-                          ),
-                          padding: EdgeInsets.all(10),
-                          child: Text(
-                            '\$' + e.amount.toString(),
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 20,
-                              color: Colors.purple,
-                            ),
-                          ),
-                        ),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              e.title,
-                              style: TextStyle(
-                                fontSize: 16,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            //  Text(e.date.toString(),
-                            //       style: TextStyle(
-                            //         color: Colors.grey,
-                            //       ),)
-                            Text(
-                              DateFormat.yMd().format(
-                                  e.date), //add intl package to format the date
-                              style: TextStyle(
-                                color: Colors.grey,
-                              ),
-                            )
-                          ],
-                        )
-                      ],
-                    ))
-                .toList(),
-          ),
+          
+          TransactionList(),
         ],
       ),
     );
--- lib/widjet/transaction_list.dart
+++ lib/widjet/transaction_list.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:personal_expenses/model/transaction.dart';
 
 class TransactionList extends StatefulWidget {
@@ -23,6 +24,59 @@ class _TransactionListState extends State<TransactionList> {
   ];
   @override
   Widget build(BuildContext context) {
-    return Container();
+    // Card(child: Text(transactions[0].title),)  // to display the transactions >> static
+    return Column(
+            children: transactions
+                .map((e) => // to display the transactions >> dynamic
+                    Row(
+                      children: [
+                        Container(
+                          margin: EdgeInsets.symmetric(
+                            vertical: 10,
+                            horizontal: 15,
+                          ),
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                              color: Colors.purple,
+                              width: 2,
+                            ),
+                          ),
+                          padding: EdgeInsets.all(10),
+                          child: Text(
+                            '\$' + e.amount.toString(),
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                              color: Colors.purple,
+                            ),
+                          ),
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              e.title,
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            //  Text(e.date.toString(),
+                            //       style: TextStyle(
+                            //         color: Colors.grey,
+                            //       ),)
+                            Text(
+                              DateFormat.yMd().format(
+                                  e.date), //add intl package to format the date
+                              style: TextStyle(
+                                color: Colors.grey,
+                              ),
+                            )
+                          ],
+                        )
+                      ],
+                    ))
+                .toList(),
+          );
   }
 }
